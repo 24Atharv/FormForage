f14ebe191741b127fc52d6b3e5585a27a56d4ec5
atharv <atharvnandurkar@gmail.com>
2025-09-14 16:26:23 +0530

    starter code backend

---
+++ apps/backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { Router } from "express";
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("Server start");
+})
+
